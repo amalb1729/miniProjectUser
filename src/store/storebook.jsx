101a736171beac5fe457e8679371e79c96d02f6f
amal <amalvani2003@gmail.com>
2025-01-30 18:45:10 +0530

    store reworked

--- src/store/storebook.jsx
+++ src/store/storebook.jsx
@@ -1,33 +1,27 @@
-
-
 import "./storebook.css"
 
-function Store(){
+function Store() {
+  const items = [
+    { name: "Fair Record", price: "₹100" },
+    { name: "Fair Record (with graph)", price: "₹110" },
+    { name: "Lab Manual", price: "₹80" },
+    { name: "Rough Record", price: "₹50" },
+    { name: "Rough Record Ruled", price: "₹60" },
+    { name: "Notebook", price: "₹50" }
+  ];
 
-    const items = [
-        { name: "Fair Record", price: "₹100" },
-        { name: "Fair Record (with graph)", price: "₹110" },
-        { name: "Lab Manual", price: "₹80" },
-        { name: "Rough Record", price: "₹50" },
-        { name: "Rough Record Ruled", price: "₹60" },
-        { name: "Notebook", price: "₹50" }
-      ];
-
-    return(
-        <>
-        <div className="cardContainer">
-
-            {items.map((item,index)=>(
-            <div className="card" key={index}>
-                    <img src="https://placehold.co/100"></img>
-                    <h3>{item.name}</h3>
-                    <p>{item.price}</p>
-                <button type="button" className="bookBtn">Book now</button>
-            </div>
-            ))}
+  return (
+    <div className="cardContainer">
+      {items.map((item, index) => (
+        <div className="card" key={index}>
+          <img src="https://placehold.co/100" alt={item.name} />
+          <h3>{item.name}</h3>
+          <p>{item.price}</p>
+          <button type="button" className="bookBtn">Book now</button>
         </div>
-        </>
-    )
+      ))}
+    </div>
+  );
 }
 
-export default Store
+export default Store;
