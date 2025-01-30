53fc5ec4f5df305f3baf4b4bc31229001833ff7d
amal <amalvani2003@gmail.com>
2025-01-30 18:40:11 +0530

    profile reworked

--- src/profile/Profile.jsx
+++ src/profile/Profile.jsx
@@ -1,37 +1,26 @@
 import { useState } from "react"
 import "./profile.css"
-function Profile(){
 
+function Profile() {
+    const [stname, setStname] = useState("sample_name")
+    const [stdep, setStdep] = useState("sample_dep")
+    const [styear, setStyear] = useState("sample_year")
 
-        const [stname,setStname]=useState("sample_name")
-        const [stdep,setStdep]=useState("sample_dep")
-        const [styear,setStyear]=useState("sample_year")
-
-        return (
-            <>
-
-            <div className="profile">
-                <div className="profileDetails">
-
-                    <img src="https://placehold.co/100"></img>
-                    <p>Name:{stname}</p>
-                    <p>Department:{stdep}</p>
-                    <p>Year:{styear}</p>
-
-                </div>
-
-
-                <div className="profieQr">
-                    <h2>QR code</h2>
-                    <img src="https://placehold.co/100"></img>
-                </div>
+    return (
+        <div className="profile">
+            <div className="profileDetails">
+                <img src="https://placehold.co/100" alt="Profile" />
+                <p>Name: {stname}</p>
+                <p>Department: {stdep}</p>
+                <p>Year: {styear}</p>
             </div>
 
-            </>
-        )
-
+            <div className="profieQr">
+                <h2>QR Code</h2>
+                <img src="https://placehold.co/100" alt="QR Code" />
+            </div>
+        </div>
+    )
 }
-
-
 
 export default Profile
